6197c5f33cd415c5e34d148716d18b28915095a5
GeraLZ <gera.lz2305@gmail.com>
2020-04-23 08:39:01 -0500

    Primera version del programa encargado de monitorizar. Lenguaje Python

---
+++ Codigo_Monitor/monitor.py
@@ -0,0 +1,40 @@
+import psutil
+import platform
+from datetime import datetime
+
+def uso_memoria():
+    memoria=psutil.virtual_memory().percent
+    return memoria
+
+def uso_cpu():
+    cpu = psutil.cpu_percent()
+    return cpu
+
+def uso_disco():
+    particiones =  psutil.disk_partitions()
+    for particion in particiones:
+        try:
+            partition_usage = psutil.disk_usage(particion.mountpoint)
+        except PermissionError:
+            continue
+        disco = partition_usage.percent
+        return disco
+
+def uso_discoT():
+    discoT= psutil.disk_usage("/").percent
+    return discoT
+
+
+
+if __name__ == '__main__':
+    print ("="*20 , " USO TOTAL DE MEMORIA ", "="*20, "\n")
+    print ("Uso total de Memoria: ", uso_memoria(), "%", "\n")
+
+    print ("="*20 , " USO TOTAL DE CPU ", "="*20, "\n")    
+    print ("Uso total de CPU: " ,uso_cpu(), "%", "\n" )
+
+    print ("="*20 , " USO TOTAL DE DISCO ", "="*20, "\n")
+    print ("Uso total de Disco: ", uso_discoT(), "%", "\n")
+
+    print ("="*20 , " USO DE DISCO ", "="*20, "\n")
+    print ("Uso total de disco: " ,uso_disco(), "%", "\n")
