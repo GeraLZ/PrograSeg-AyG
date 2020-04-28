0759f55f648fc1d93f3e0ab8ca33cce67b0ad35c
adaLuCS <adarelycs@gmail.com>
2020-04-28 18:15:26 -0500

    Se hizo un cambio al archivo de settings.py

--- monitor/monitor/settings.py
+++ monitor/monitor/settings.py
@@ -21,7 +21,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY')
-#'0ftha3b4hbgbu)jvxpm(_-xj-_q)$opj52z=+7gtqu5#&jhy53'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
