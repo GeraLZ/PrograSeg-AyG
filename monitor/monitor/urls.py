5e438c849e1d3a172942fdd7e4eaea1cb59b0f4d
adaLuCS <adarelycs@gmail.com>
2020-05-06 21:16:38 -0500

    Se importo las views de monitor y se agregaron los paths de inicioSesion y inicioAdmin

--- monitor/monitor/urls.py
+++ monitor/monitor/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from monitor.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('inicioSesion/', inicioSesion),
+    path('paginaInicioAdmin/', paginaInicioAdmin),
 ]
