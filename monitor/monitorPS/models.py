22ed4e9ffd6fb7863be806fe6eafc9fe00102bc2
GeraLZ <gera.lz2305@gmail.com>
2020-07-07 18:33:38 -0500

    Revert "Se modifico el archivo de models.py"
    This reverts commit 616cb6b4cf7ccc21c7fd806683fe2f9e895c1c08.

--- monitor/monitorPS/models.py
+++ monitor/monitorPS/models.py
@@ -6,20 +6,12 @@ import base64
 class admin(models.Model):
     usuario = models.CharField(max_length=15, default=None)
     password = models.CharField(max_length=100, default=None)
-    salt = models.CharField(max_length=25, default=None)
+    salt = models.CharField(max_length=25, default=(base64.b64encode(os.urandom(16))).decode('utf-8'))
     id_token = models.CharField(max_length=50, default=None)
     id_chat = models.CharField(max_length=10, default=None)
-    codigo_token = models.CharField(max_length=25, default=None,blank=True,null=True)
+    codigo_token = models.CharField(max_length=25, default=None)
 
 class ips(models.Model):
     ip = models.GenericIPAddressField(null=False, blank=False, unique=True)
     ultima_peticion = models.DateTimeField(null=False, blank=False)
     intentos = models.IntegerField(null=False, blank=False, default=0)
-
-class user(models.Model):
-    usuario = models.CharField(max_length=15, default=None)
-    password = models.CharField(max_length=100, default=None)
-    salt = models.CharField(max_length=25, default=(base64.b64encode(os.urandom(16))).decode('utf-8'))
-    id_token = models.CharField(max_length=50, default=None)
-    id_chat = models.CharField(max_length=10, default=None)
-    codigo_token = models.CharField(max_length=25, default=None,blank=True,null=True)
