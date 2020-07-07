b4509eedacc9bbe9ecc8305bc6290b14c63572bb
GeraLZ <gera.lz2305@gmail.com>
2020-07-07 18:40:45 -0500

    Modificaciones carpeta models, cambios en models.py a clases admin e ips, creacion class user

--- monitor/monitorPS/models.py
+++ monitor/monitorPS/models.py
@@ -5,13 +5,21 @@ import base64
 # Create your models here.
 class admin(models.Model):
     usuario = models.CharField(max_length=15, default=None)
-    password = models.CharField(max_length=100, default=None)
-    salt = models.CharField(max_length=25, default=(base64.b64encode(os.urandom(16))).decode('utf-8'))
+    password = models.CharField(max_length=110, default=None)
+    salt = models.CharField(max_length=25, default=None)
     id_token = models.CharField(max_length=50, default=None)
     id_chat = models.CharField(max_length=10, default=None)
-    codigo_token = models.CharField(max_length=25, default=None)
+    codigo_token = models.CharField(max_length=25, default=None,blank=True,null=True)
 
 class ips(models.Model):
     ip = models.GenericIPAddressField(null=False, blank=False, unique=True)
     ultima_peticion = models.DateTimeField(null=False, blank=False)
     intentos = models.IntegerField(null=False, blank=False, default=0)
+
+class user(models.Model):
+    usuario = models.CharField(max_length=15, default=None)
+    password = models.CharField(max_length=110, default=None)
+    salt = models.CharField(max_length=25, default=None)
+    id_token = models.CharField(max_length=50, default=None)
+    id_chat = models.CharField(max_length=10, default=None)
+    codigo_token = models.CharField(max_length=25, default=None,blank=True,null=True)
