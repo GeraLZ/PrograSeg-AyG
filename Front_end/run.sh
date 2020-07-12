ce17f98ff0d83029d9dfa048e97dba117a435f66
adaLuCS <adarelycs@gmail.com>
2020-07-12 02:14:10 -0500

    Parte de Administrador Global terminada Frontend

--- Front_end/run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-sleep 15
-
-su -c 'python3 -u manage.py makemigrations' limitado
-su -c 'python3 -u manage.py migrate' limitado
-
-su -c 'gunicorn --bind :8000 monitor.wsgi:application --reload' limitado
--- Front_end/up_services.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-for var in $(ccrypt -d -c settings.env.cpt); do
-    export "$var"
-done
-
-docker-compose up -d
